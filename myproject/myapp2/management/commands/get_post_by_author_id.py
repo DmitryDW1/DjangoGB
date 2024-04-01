78384c2f83e0426eaee5588aa1b5d986ed1ece7f
DmitryDW <DmitryDW1@yandex.ru>
2024-04-01 21:58:16 +0600

    Доделал лекцию

---
+++ myproject/myapp2/management/commands/get_post_by_author_id.py
@@ -0,0 +1,36 @@
+from django.core.management.base import BaseCommand
+from myapp2.models import Author, Post
+
+
+class Command(BaseCommand):
+    help = "Get all posts by author id."
+    
+    def add_arguments(self, parser):
+        parser.add_argument('pk', type=int, help='User ID')
+        
+    # def handle(self, *args, **kwargs):
+    #     pk = kwargs.get('pk')
+    #     author = Author.objects.filter(pk=pk).first()
+    #     if author is not None:
+    #         posts = Post.objects.filter(author=author)
+    #         intro = f'All posts of {author.name}\n'
+    #         text = '\n'.join(post.content for post in posts)
+    #         self.stdout.write(f'{intro}{text}')
+    '''
+    Метод вывода постов юзера без обращения к имени юзера (только по id)
+    '''
+    # def handle(self, *args, **kwargs):
+    #     pk = kwargs.get('pk')
+    #     posts = Post.objects.filter(author__pk=pk)
+    #     intro = f'All posts of\n'
+    #     text = '\n'.join(post.content for post in posts)
+    #     self.stdout.write(f'{intro}{text}')
+    '''
+    Пользоательский метод вывода данных
+    '''
+    def handle(self, *args, **kwargs):
+        pk = kwargs.get('pk')
+        posts = Post.objects.filter(author__pk=pk)
+        intro = f'All posts of\n'
+        text = '\n'.join(post.get_summary() for post in posts)
+        self.stdout.write(f'{intro}{text}')
